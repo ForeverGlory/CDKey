9bf89c1c71694b0a94d3d1d521a309bee69a4d35
ForeverGlory <foreverglory@qq.com>
2014-11-26 00:40:44 +0800

    新增 api，代码分离

--- cdkey.api.php
+++ cdkey.api.php
@@ -1,2 +1,14 @@
 <?php
 
+function hook_cdkey_info() {
+  return array(
+    'sample' => array(
+      'name' => 'sample',
+      'description' => 'This is sample description'
+    )
+  );
+}
+
+function hook_cdkey_info_alter(&$types) {
+  
+}
